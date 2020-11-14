d1c2bffead4d40fed5031b07538c2ee879161cdb
Andrii Potapenko <andriy.potapenko@trov.com>
2020-11-14 14:15:24 +0000

    Add test application and library modules

---
+++ app/src/main/java/com/parrotland94/datepicker/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.parrotland94.datepicker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
